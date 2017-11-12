d5b86505120a60150b048b4c9401bb5da84e10f5
Bron Gondwana <brong@fastmail.fm>
2017-11-12 18:13:52 +1100

    version 0.11

--- lib/Net/CalDAVTalk.pm
+++ lib/Net/CalDAVTalk.pm
@@ -413,11 +413,11 @@ Net::CalDAVTalk - Module to talk CalDAV and give a JSON interface to the data
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 
 =head1 SYNOPSIS
